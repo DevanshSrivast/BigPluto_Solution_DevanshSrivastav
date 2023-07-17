32d5750f226e53b0f74b77106838046bdd537e77
DevanshSrivast <139769223+DevanshSrivast@users.noreply.github.com>
2023-07-17 17:29:46 +0530

    Create Q2.java

---
+++ Q2.java
@@ -0,0 +1,13 @@
+//Ques-02
+
+public class Ques2{
+  public static void main(String args[]){
+
+    if({} && () && []){
+        System.out.println("The Brackets are in balanced form");
+    }
+    else([}] && {[} && [{]){
+        System.out.println("The Brackets are in Unbalanced form");
+    }
+  }
+}
