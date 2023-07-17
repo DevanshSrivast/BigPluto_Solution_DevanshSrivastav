6fc2ebc8d0a4b006942c33c9f2984290238e3c1d
DevanshSrivast <139769223+DevanshSrivast@users.noreply.github.com>
2023-07-17 17:28:54 +0530

    Create Q1.java

---
+++ Q1.java
@@ -0,0 +1,22 @@
+//Ques-01 
+
+import java.util.Scanner(System.in);
+public class index{
+    public static void main(String args[]){
+       
+      int N1=6;
+      int a[]={1,2,3,4,6};
+      for(int i=1;i<=N1;i++){
+
+      
+          if(a[i]<N1){
+            System.out.println();
+          }
+          else{
+            System.out.println(a[i]);
+          }
+          
+        }
+
+    }
+}
